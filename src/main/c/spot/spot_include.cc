e94579f615e06343d71c2af0d8df0f1b590ff160
Yong Li <duckly_lee@163.com>
2019-01-07 17:08:28 +0800

    add functions to manipulate twas (not working correctly yet)

--- src/main/c/spot/spot_include.cc
+++ src/main/c/spot/spot_include.cc
@@ -68,26 +68,14 @@ product(spot::twa_graph_ptr left, spot::twa_graph_ptr right)
   return spot::product(left, right);
 }
 
-extern "C"
-const char*
-included()
-{
-	return "Included.";
-}
+typedef struct result{
+	string ce;
+} result;
 
-extern "C" 
-const char*
-not_included()
-{
-	return "Not included: ";
-}
+typedef result* result_ptr;
 
-extern "C" 
-const char*
-cycle()
-{
-	return "cycle";
-}
+const char* str_included = "Included.";
+const char* str_not_included = "Not included: ";
 
 static string
 is_included(spot::twa_graph_ptr aut_a, spot::twa_graph_ptr aut_b) 
@@ -96,38 +84,223 @@ is_included(spot::twa_graph_ptr aut_a, spot::twa_graph_ptr aut_b)
 	spot::twa_word_ptr word = aut_a->intersecting_word(complement_aut_b);
 	std::stringstream ss;
 	if(word != nullptr) {
-		ss << not_included();
+		ss << str_not_included;
 		//spot::twa_graph_ptr result = word->as_automaton();
 		word->simplify();
 		ss << (*word);
 		 // Print the resulting automaton.
 		//print_hoa(std::cout, result);
     }else {
-		ss << included();
+		ss << str_included;
 	}
 	return ss.str();
 }
 
-extern "C" 
-const char *
-is_included(const char* file_a, const char* file_b) 
+extern "C" {
+    
+    
+__attribute__ ((visibility("default")))
+result*
+new_ce() 
+{
+	return new result;
+}
+
+__attribute__ ((visibility("default")))
+int 
+size_ce(result_ptr rt)
+{
+	return (rt->ce).length();
+}
+
+__attribute__ ((visibility("default")))
+char
+get_ce_char(result_ptr rt, int i) 
+{
+	return (rt->ce)[i];
+}
+
+__attribute__ ((visibility("default")))
+void 
+free_ce(result_ptr rt) 
+{
+	if(rt != nullptr){
+		delete rt;
+	}
+}
+
+__attribute__ ((visibility("default")))
+void
+is_included(result_ptr rt, const char* file_a, const char* file_b) 
 {    
 	spot::bdd_dict_ptr dict = spot::make_bdd_dict();
 	auto aut_a = read_automaton(file_a, dict);
 	auto aut_b = ensure_deterministic(read_automaton(file_b, dict), true);
     string str = is_included(aut_a, aut_b);
-    char* result = (char*) malloc(str.length());
-    strcpy (result, str.c_str());
-    return result;
+	rt->ce = str;
+}
+
+__attribute__ ((visibility("default")))
+const char*
+cycle()
+{
+	return "cycle";
+}
+
+__attribute__ ((visibility("default")))
+const char*
+not_included()
+{
+	return str_not_included;
+}
+
+__attribute__ ((visibility("default")))
+const char*
+included()
+{
+	return str_included;
+}
+
+// --------------------- interface for constructing nba
+
+__attribute__ ((visibility("default")))
+void
+aut_is_included(result_ptr rt, spot::twa_graph_ptr aut_a, spot::twa_graph_ptr aut_b) 
+{
+	aut_b = ensure_deterministic(aut_b, true);
+    string str = is_included(aut_a, aut_b);
+	rt->ce = str;
+}
+
+// create an alphabet
+__attribute__ ((visibility("default")))
+spot::bdd_dict_ptr
+aut_new_dict()
+{
+	return spot::make_bdd_dict();
+}
+
+// create an automaton
+__attribute__ ((visibility("default")))
+spot::twa_graph_ptr
+aut_new_twa(spot::bdd_dict_ptr dict)
+{
+	return make_twa_graph(dict);
+}
+
+// register a proposition
+__attribute__ ((visibility("default")))
+int
+aut_add_prop(spot::twa_graph_ptr aut, const char* ap)
+{
+	return aut->register_ap(ap);
+}
+
+__attribute__ ((visibility("default")))
+void
+aut_set_acceptance(spot::twa_graph_ptr aut, int numOfIndex, const char* acc)
+{
+	aut->set_acceptance(numOfIndex, acc);
+}
+
+__attribute__ ((visibility("default")))
+void 
+aut_new_states_num(spot::twa_graph_ptr aut, int num)
+{
+	aut->new_states(num);
+}
+
+__attribute__ ((visibility("default")))
+void
+aut_set_init_state(spot::twa_graph_ptr aut, int init)
+{
+	aut->set_init_state(init);
+}
+
+__attribute__ ((visibility("default")))
+void
+aut_new_edge(spot::twa_graph_ptr aut, int s, int t, const bool* ap, int size_ap)
+{
+	bdd label = bddfalse;
+	for(int i = 0; i < size_ap; i ++) 
+	{
+		bdd lit = ap[i] ? bdd_ithvar(i) : ! bdd_ithvar(i);
+		if(i == 0) 
+		{
+			label = lit;
+		}else 
+		{
+			label = label & lit;
+		}
+	}
+	aut->new_edge(s, t, label);
+}
+
+__attribute__ ((visibility("default")))
+void
+aut_new_true_edge(spot::twa_graph_ptr aut, int s, int t)
+{
+	aut->new_edge(s, t, bddtrue);
+}
+
+__attribute__ ((visibility("default")))
+void
+aut_new_edge_label(spot::twa_graph_ptr aut, int s, int t, const bool* ap, int size_ap, const int* marks, int size_marks)
+{
+	// first convert it to acc_cond::mark_t
+	spot::acc_cond::mark_t m = {};
+    for(int i = 0; i < size_marks; i ++) 
+    {
+		m.set(marks[i]);
+	}
+	bdd label = bddfalse;
+	for(int i = 0; i < size_ap; i ++) 
+	{
+		bdd lit = ap[i] ? bdd_ithvar(i) : ! bdd_ithvar(i);
+		if(i == 0) 
+		{
+			label = lit;
+		}else 
+		{
+			label = label & lit;
+		}
+	}
+	aut->new_edge(s, t, label, m);
+}
+
+__attribute__ ((visibility("default")))
+void
+aut_new_true_edge_label(spot::twa_graph_ptr aut, int s, int t, const int* marks, int size_marks)
+{
+	spot::acc_cond::mark_t m = {};
+    for(int i = 0; i < size_marks; i ++) 
+    {
+		m.set(marks[i]);
+	}
+	aut->new_edge(s, t, bddtrue, m);
+}
+
+// -------------- interface for visiting an automaton
+__attribute__ ((visibility("default")))
+bool 
+aut_is_empty(spot::twa_graph_ptr aut)
+{
+	return aut->is_empty();
 }
 
 
+// end of extern
+}
+	
+
 int main(int argc, char** args) 
 {
+	/*
 	if(argc < 1 || argc > 4) {
 		cerr<<"please input two automata: " << argc;
 		return -1;
 	}
+	
 	spot::bdd_dict_ptr dict = spot::make_bdd_dict();
 	auto autB = ensure_deterministic(read_automaton(args[2], dict), true);
     auto autA = read_automaton(args[1], dict);
@@ -135,9 +308,69 @@ int main(int argc, char** args)
     cout << ce.c_str() << "\n";
     cout << args[1] << endl;
     cout << args[2] << endl;
-    const char* rt = is_included(args[1], args[2]);
-    cout << rt << endl;
-    cout << included() << endl;
-    cout << not_included() << endl;
+    cout << "-------------------" << endl;
+    result_ptr rt = new_ce();
+    is_included(rt, args[1], args[2]);
+    cout << "-------------------" << size_ce(rt) << endl;
+
+    for(int i = 0; i < size_ce(rt); i ++) {
+		//cout << get_ce_char(rt, i) << endl;
+	}
+	cout << "-2";
+	free_ce(rt);
+	*/
+	cout << "-1";
+	// test automaton construction
+	spot::bdd_dict_ptr dd = aut_new_dict();
+	cout << "0";
+	
+	// This creates an empty automaton that we have yet to fill.
+	spot::twa_graph_ptr aut = aut_new_twa(dd);
+    cout << "1";
+    
+	int a = aut_add_prop(aut, "a");
+	cout << "2 -> " << a << endl;
+	
+	// Set the acceptance condition of the automaton to co-Büchi
+	aut_set_acceptance(aut, 1, "Inf(0)");
+	cout << "3";
+	
+	// States are numbered from 0.
+	aut_new_states_num(aut, 3);
+	cout << "4";
+	
+  // The default initial state is 0, but it is always better to
+  // specify it explicitely.
+	aut_set_init_state(aut, 0U);
+	cout << "5";
+	
+  // new_edge() takes 3 mandatory parameters: source state,
+  // destination state, and label.  A last optional parameter can be
+  // used to specify membership to acceptance sets.
+  //
+  // new_univ_edge() is similar, but the destination is a set of
+  // states.
+	bool* mark = new bool[1];
+	mark[0] = true;
+    cout << "6";
+	aut_new_edge(aut, 0, 0, mark, 1);
+	cout << "7";
+	int* m = new int[1];
+	m[0] = 0;
+	mark[0] = false;
+	aut_new_edge_label(aut, 1, 1, mark, 1, m, 1);
+		mark[0] = true;
+
+	cout << "8";
+	aut_new_edge(aut, 1, 2, mark, 1);
+	cout << "9";
+	
+	aut_new_edge(aut, 2, 2, mark, 1);
+	cout << "10";
+	mark[0] = false;
+	aut_new_edge(aut, 2, 2, mark, 1);
+	cout << "11" << endl;
+	// Print the resulting automaton.
+	print_hoa(std::cout, aut);
 	return 0;
 }
--- src/main/java/spotj/SpotJ.java
+++ src/main/java/spotj/SpotJ.java
@@ -1,38 +1,119 @@
 package spotj;
 
+import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.Pointer;
 
-public class SpotJ implements SpotLibrary {
+public class SpotJ {
 	
 	private SpotLibrary spot;
+	public static final String INCLUDED = "Included.";
+	public static final String NOT_INCLUDED = "Not included: ";
+	public static final String CYCLE = "cycle";
 	
 	public SpotJ() {
 		this.spot = Native.load("spotj", SpotLibrary.class);
 	}
 	
+	public static void main(String[] args) {
+		final SpotJ spot = new SpotJ();
+		String result = spot.is_included("/home/liyong/git/spot/A.hoa", "/home/liyong/git/spot/B.hoa");
+		System.out.printf("%s\n", result);
+	}
+	
+	public SpotLibrary getSpot() {
+		return spot;
+	}
+	
 	public String is_included(String a, String b) {
-		return spot.is_included(a, b);
+		Pointer rt = spot.new_ce();
+		spot.is_included(rt, a, b);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < spot.size_ce(rt); i ++) {
+			sb.append("" + spot.get_ce_char(rt, i));
+		}
+		spot.free_ce(rt);
+		return sb.toString();
 	}
 
-	public String included() {
-		return spot.included();
+	public String getIncluded() {
+		return INCLUDED;
 	}
 
-	public String not_included() {
-		return spot.not_included();
+	public String getNotincluded() {
+		return NOT_INCLUDED;
 	}
 
 	public String cycle() {
-		return spot.cycle();
+		return CYCLE;
 	}
 	
-	public static void main(String[] args) {
-		final SpotJ spot = new SpotJ();
-		String result = spot.is_included("/home/liyong/git/spot/B.hoa", "/home/liyong/git/spot/B.hoa");
-		System.out.printf("%s\n", result);
-		System.out.printf("%s\n", spot.included());
-		System.out.printf("%s\n", spot.not_included());
-	}
+	/**
+	 * spot library
+	 * 
+	 * */
+	public static interface SpotLibrary extends Library
+	{
+		/**
+		 * structure to put counterexample
+		 * */
+		Pointer new_ce();
+		/**
+		 * check whether aut a is included in automaton b
+		 * */
+	    void is_included(Pointer rt, String a, String b);
+	    /**
+		 * size of counterexample
+		 * */
+	    int size_ce(Pointer rt);
+	    /**
+		 * get character at i of counterexample
+		 * */
+	    char get_ce_char(Pointer rt, int i);
+	    
+	    /**
+	     * clean the memory
+	     * **/
+	    void free_ce(Pointer rt);
+	    
+	    // interface for manipulating automata in spot
+	    /*
+	    void aut_is_included(Pointer rt, Pointer aut_a, Pointer aut_b);
 
+	    // create an alphabet
+	    Pointer aut_new_dict();
+
+	    // create an automaton
+	    Pointer aut_new_twa(Pointer dict);
+
+	    // register a proposition
+	    // cause fatal error in runtime
+	    int aut_add_prop(Pointer aut, String ap);
+
+	    // set acceptance acc = "Inf(0)"
+	    void aut_set_acceptance(Pointer aut, int numOfIndex, String acc);
+
+	    // state number
+	    void aut_new_states_num(Pointer aut, int num);
+
+	    // initial state
+	    void aut_set_init_state(Pointer aut, int init);
+
+	    // neg indicates whether add the negation of ap
+	    void aut_new_edge(Pointer aut, int s, int t, boolean[] ap, int size_ap);
+
+	    // add a true label on the transition
+	    void aut_new_true_edge(Pointer aut, int s, int t);
+
+	    // add a marked label on transition
+	    void aut_new_edge_label(Pointer aut, int s, int t, boolean[] ap, int size_ap, int[] marks, int size_marks);
+
+	    // add a makred true label
+	    void aut_new_true_edge_label(Pointer aut, int s, int t, int[] marks, int size_marks);
+
+	    // -------------- interface for visiting an automaton
+	    boolean aut_is_empty(Pointer aut);
+		*/
+	}
 
 }
